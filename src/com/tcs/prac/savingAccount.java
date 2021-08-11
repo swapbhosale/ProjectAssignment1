3cf1b4f71aa84ce955072c5855fe4eab3de24369
swapbhosale <swapiiibhosale@gmail.com>
2021-08-11 12:31:26 +0000

    project

--- src/com/tcs/prac/savingAccount.java
+++ src/com/tcs/prac/savingAccount.java
@@ -1,5 +1,11 @@
 package com.tcs.prac;
 
 public class savingAccount {
+	public static void main(String[] args) {
+		public  savingAccount(String s) {
+			this.name=s;
+			System.out.println("saving"+swap);
+		}
+	}
 
 }
