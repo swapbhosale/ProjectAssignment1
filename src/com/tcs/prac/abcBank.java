433d9cf16fd5a93968056ce5e451bf21f75a7a18
swapbhosale <swapiiibhosale@gmail.com>
2021-08-17 07:15:23 +0000

    assignment

--- src/com/tcs/prac/abcBank.java
+++ src/com/tcs/prac/abcBank.java
@@ -24,10 +24,45 @@ public class abcBank {
 		return a;
 
 	}
-	
-	public static void main(String[] args) {
+	public static void deactivate(HashMap <String,Account> hm,Set<String> key ,Collection<Account> values) {
+		
 		Scanner sc=new Scanner(System.in);
-		HashMap <String,Account> hm = new HashMap();
+		System.out.println("want to  deactivate account ?");
+		String d = sc.nextLine();
+		if(d.equals("yes")) {
+			System.out.println("Enter the name of accout to deactivate ");
+			String f=sc.nextLine();
+			if(key.contains(f) && (hm.get(f).getStatus())=="active") {
+				Account g=hm.get(f);
+				g.setStatus("deactive");
+				System.out.println("Deactivated");
+			}
+			else {
+				System.out.println("Accout is already deactivated");
+			}
+			
+		}
+	}
+	public static void activate(HashMap <String,Account> hm,Set<String> key ,Collection<Account> values){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("want to  Activate account ?");
+		String d1 = sc.nextLine();
+		if(d1.equals("yes")) {
+			System.out.println("Enter the name of accout to Activate ");
+			String f=sc.nextLine();
+			if(key.contains(f) && (hm.get(f).getStatus())=="deactive") {
+				Account g=hm.get(f);
+				g.setStatus("active");
+				System.out.println("activated");
+			}
+			else {
+				System.out.println("Accout is already activated");
+			}
+			
+		}
+	}
+	public static void main(String[] args) {
+		HashMap <String,Account> hm = new HashMap<String, Account>();
 		Account A1 =new Account(); 
 		//Account A2 =new Account();
 		A1=createAccount();
@@ -38,21 +73,8 @@ public class abcBank {
 		Collection<Account> values = (hm.values());
 		System.out.println("key : " +key);
 		System.out.println("values : "+values);
-		System.out.println("want to  deactivate account ?");
-		String d = sc.nextLine();
-		if(d.equals("yes")) {
-			System.out.println("Enter the name of accout to deactivate ");
-			String f=sc.nextLine();
-			if(key.contains(f)) {
-				Account g=hm.get(f);
-				g.setStatus("Deactivate");
-				System.out.println("Deactivated");
-				
-				
-			}
-		System.out.println(A1.getStatus());
-			
-		}
+		deactivate(hm,key,values);
+		activate(hm,key,values);
 	}
 
 }
